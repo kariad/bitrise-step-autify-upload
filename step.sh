415ade6711c6fc3bdd79d22914e5ba56ae70fbea
tarappo <tarappo@gmail.com>
2021-12-15 05:24:01 +0900

    use local shell script

--- step.sh
+++ step.sh
@@ -7,15 +7,11 @@ set -e
 export AUTIFY_UPLOAD_TOKEN="$upload_token"
 export AUTIFY_PROJECT_ID="$project_id"
 export AUTIFY_APP_DIR_PATH="$app_dir_path"
-export AUTIFY_MOBILE_SCRIPT="https://raw.githubusercontent.com/autifyhq/autify-for-mobile-cli/main/autify_mobile_cli.sh"
 export AUTIFY_MOBILE_SCRIPT_NAME="autify_mobile_cli.sh"
 
 readonly API_BASE_ADDRESS="https://mobile-app.autify.com/api/v1"
 readonly WORKIND_DIR="./"
 readonly ZIP_NAME="upload.zip"
 
-# download script
-curl -L -o "$WORKIND_DIR/$AUTIFY_MOBILE_SCRIPT_NAME" $AUTIFY_MOBILE_SCRIPT
-chmod 777 "$WORKIND_DIR/$AUTIFY_MOBILE_SCRIPT_NAME"
 
 exec "$WORKIND_DIR/$AUTIFY_MOBILE_SCRIPT_NAME"
